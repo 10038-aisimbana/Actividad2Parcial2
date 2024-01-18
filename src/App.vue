3b9295917319234f43f7c6b78ea8e8c56f555e22
10038-aisimbana <93232965+10038-aisimbana@users.noreply.github.com>
2024-01-17 22:27:52 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <h1 class="title">Actividad 2</h1>
+    <p class="info">Nombre: Adrian Simbaña</p>
+    <p class="info">14386</p><br>
+    <CustomInput v-model="message" />
+  </div>
+</template>
+
+<script>
+import CustomInput from './components/CustomInput.vue';
+
+export default {
+  components: {
+    CustomInput
+  },
+  data() {
+    return {
+      message: 'Mensaje inicial'
+    };
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  margin-top: 20px;
+}
+
+.title {
+  font-family: 'Arial', sans-serif;
+  font-size: 28px;
+  margin-bottom: 10px;
+}
+
+.info {
+  font-family: 'Arial', sans-serif;
+  font-size: 16px;
+  margin: 5px 0;
+}
+</style>
---
+++ src/components/CustomInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <input v-model="userInput" placeholder="Ingrese un mensaje o texto por favor" size="29">
+      <p>{{ userInput }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        userInput: ''
+      };
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .custom-input {
+    max-width: 300px;
+    margin: auto;
+    text-align: center;
+  }
+  
+  .input-field {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+  }
+  
+  /* Estilos adicionales según sea necesario */
+  </style>
